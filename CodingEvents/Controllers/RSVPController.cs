29e4a5835acb484645d2e908c8b01334b388b1a2
Adam Bailey <abailey76@duck.com>
2021-12-13 18:04:18 -0600

    trying to add RSVP list to exercises

---
+++ CodingEvents/Controllers/RSVPController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CodingEvents.Controllers
+{
+    public class RSVPController : Controller
+    {
+        static private Dictionary<string, string> Attendees = new Dictionary<string, string>();
+
+        //GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Attendees;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/RSVP/Add")]
+        public IActionResult RSVP(string name, string response)
+        {
+            Attendees.Add(name, response);
+            return Redirect("/RSVP");
+        }
+    }
+}
